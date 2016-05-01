393481c363781fe3365f3a26c0bb6f1e656cd6bb
yanwuguo <goanve@gmail.com>
2016-05-01 19:58:01 +0200

    Create peptcal.py

---
+++ peptcal.py
@@ -0,0 +1,9 @@
+## peptide properties calculator.
+## http://biotools.nubic.northwestern.edu/proteincalc.html
+
+#aa_dict = { "A":[name, 3 letter code, MW,Hydropathy] }
+# return lenght, MW, Approximate Volume, 3 letter code
+# 3 to 1 convertor.
+# percentage of each aa with 3 letter code
+# soluble, 
+## blast domain?
